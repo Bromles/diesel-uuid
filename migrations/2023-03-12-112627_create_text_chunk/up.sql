7d2fab7cbe8281e3e2a38486738ba47b7359ea5c
bromles <all.bromles@gmail.com>
2023-03-12 14:57:43 +0300

    Update schema

--- migrations/2023-03-12-112627_create_text_chunk/up.sql
+++ migrations/2023-03-12-112627_create_text_chunk/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE text_chunk (
-    id uuid not null,
+    id uuid not null unique,
     text_meta_id uuid not null,
     num int4 not null,
     content text not null,
