4d858cd80531ffd0b99daa3693567ceb551d937d
bromles <all.bromles@gmail.com>
2023-03-12 16:27:59 +0300

    Update

--- migrations/2023-03-12-125354_insert_test_data/down.sql
+++ migrations/2023-03-12-125354_insert_test_data/down.sql
@@ -1,2 +1,3 @@
 -- This file should undo anything in `up.sql`
-DELETE FROM text_chunk WHERE id = '6da773bb-6257-4795-a4f5-b446729f9624'
+DELETE FROM text_chunk WHERE id = '6da773bb-6257-4795-a4f5-b446729f9624';
+DELETE FROM text_chunk WHERE id = '03f43734-2eaf-45cc-8d5e-780918124042'
--- migrations/2023-03-12-125354_insert_test_data/up.sql
+++ migrations/2023-03-12-125354_insert_test_data/up.sql
@@ -1,2 +1,3 @@
 -- Your SQL goes here
 INSERT INTO  text_chunk(id, text_meta_id, num, content) VALUES ('6da773bb-6257-4795-a4f5-b446729f9624', '0e411b6f-be41-4260-b577-ea93c8ab7634', 1, 'test');
+INSERT INTO  text_chunk(id, text_meta_id, num, content) VALUES ('03f43734-2eaf-45cc-8d5e-780918124042', '0e411b6f-be41-4260-b577-ea93c8ab7634', 2, 'test 2');
